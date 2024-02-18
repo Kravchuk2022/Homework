880c55273b0e9d91ada816d3dc9641a7fb217b14
Aleksandra <akravchuk2014@gmail.com>
2024-02-18 18:54:41 +0200

    home6

--- js_6.js
+++ js_6.js
@@ -1,7 +1,7 @@
 
 
 
-/*let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function myBlend(arr) {
     for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1)); 
@@ -10,7 +10,11 @@ function myBlend(arr) {
 }
 console.log("Початковий масив:", arr);
 myBlend(arr);
-console.log("Перемішаний масив:", arr);*/
+console.log("Перемішаний масив:", arr);
+
+
+
+
 
 const company = {
     name: 'Велика Компанія',
